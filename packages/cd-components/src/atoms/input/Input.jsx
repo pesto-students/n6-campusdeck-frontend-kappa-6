61f4e04a7ecde2e63ec28081b0a9a015c3aef2c0
PushpakB3096 <rtpushpak@gmail.com>
2021-08-16 23:10:51 +0530

    Add input types

--- packages/cd-components/src/atoms/input/Input.jsx
+++ packages/cd-components/src/atoms/input/Input.jsx
@@ -1,5 +1,26 @@
-const Input = ({ ...props }) => {
-  return <input {...props} />;
+import PropTypes from "prop-types";
+import cx from "classnames";
+
+// constants
+import INPUT_SIZE from "./constants/input.size";
+
+// styles
+import styles from "./input.module.css";
+
+const Input = ({ size, placeholder }) => {
+  const classNames = cx(styles.input, styles[size]);
+
+  return <input className={classNames} placeholder={placeholder} />;
+};
+
+Input.propTypes = {
+  size: PropTypes.string,
+  placeholder: PropTypes.string
+};
+
+Input.defaultProps = {
+  size: INPUT_SIZE.MEDIUM,
+  placeholder: undefined
 };
 
 export default Input;
--- packages/cd-components/src/index.js
+++ packages/cd-components/src/index.js
@@ -1,1 +1,2 @@
 export { default as Button } from "./atoms/button/Button";
+export { default as Input } from "./atoms/input/Input";
