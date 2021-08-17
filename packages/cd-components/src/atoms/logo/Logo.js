4fe7aeb6e43856d67d827697796b7753884a7663
PushpakB3096 <rtpushpak@gmail.com>
2021-08-17 18:55:26 +0530

    Make logo responsive

--- packages/cd-components/src/atoms/logo/Logo.js
+++ packages/cd-components/src/atoms/logo/Logo.js
@@ -1,8 +1,25 @@
+import cx from "classnames";
+import LogoImage from "./assets/logoImage.png";
+import LogoText from "./assets/logoText.png";
+
+// utility
+import { isMobile } from "@cd/base";
+
+// styles
 import styles from "./logo.module.css";
-import FullLogo from "./assets/fullLogo.png";
 
 const Logo = () => {
-  return <img className={styles.img} src={FullLogo}></img>;
+  // hide the logo text if the user is on mobile
+  const classNames = cx(styles.logo_text, {
+    [styles.hidden]: isMobile === true
+  });
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.img} src={LogoImage}></img>
+      <img className={classNames} src={LogoText}></img>
+    </div>
+  );
 };
 
 export default Logo;
