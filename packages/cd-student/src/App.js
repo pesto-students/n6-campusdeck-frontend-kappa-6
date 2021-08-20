e2ca38f58f4a94180bd8940b892e36fd8c08bee2
PushpakB3096 <rtpushpak@gmail.com>
2021-08-20 12:30:32 +0530

    Add components for testing in student module

--- packages/cd-student/src/App.js
+++ packages/cd-student/src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
-import { Button, Input } from "@cd/components";
+import {
+  Button,
+  Input,
+  LeftSidebar,
+  Post,
+  SpaceDetails,
+  SuggestionCard
+} from "@cd/components";
 
 function App() {
   return (
@@ -16,9 +23,52 @@ function App() {
       <Button text='Upload' type='skeleton' /> */}
 
       {/* Inputs */}
-      <Input size='small' placeholder='Option 1' />
+      {/* <Input size='small' placeholder='Option 1' />
       <Input size='medium' placeholder='Search posts or spaces...' />
-      <Input size='large' placeholder='Post Title' />
+      <Input size='large' placeholder='Post Title' /> */}
+
+      <LeftSidebar />
+
+      <SpaceDetails
+        name='Announcements'
+        desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt, dui a egestas luctus, lacus magna fermentum diam, ac fringilla eros ipsum sit amet risus. Vestibulum aliquet ante aliquam.'
+        numOfPosts={120}
+        followers={542}
+        numOfUsers={100}
+        creator='Pushpak Bhattacharya'
+        createdAt='1 month ago'
+      />
+
+      <Post
+        title='Lorem ipsum dolor sit amet?'
+        label='Question'
+        points='4723'
+        time='2 days ago'
+        totalComments='100+'
+        authorName='Pushpak Bhattacharya'
+      />
+
+      <SuggestionCard
+        heading='Trending Spaces'
+        list={[
+          {
+            name: "Announcements",
+            metric: "31k users"
+          },
+          {
+            name: "WebDev",
+            metric: "25k users"
+          },
+          {
+            name: "QnA",
+            metric: "21k users"
+          },
+          {
+            name: "Fests",
+            metric: "18k users"
+          }
+        ]}
+      />
     </div>
   );
 }
