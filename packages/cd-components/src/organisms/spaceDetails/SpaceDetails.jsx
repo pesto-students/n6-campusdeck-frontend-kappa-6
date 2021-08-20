24d21b195a20fb3ba0fe6a736cbb3b2d53043ee1
PushpakB3096 <rtpushpak@gmail.com>
2021-08-20 12:26:50 +0530

    Add space details card

---
+++ packages/cd-components/src/organisms/spaceDetails/SpaceDetails.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+
+import Button from "../../atoms/button/Button";
+import SpaceStats from "../../atoms/spaceStats/SpaceStats";
+
+// styles
+import styles from "./spaceDetails.module.css";
+
+const SpaceDetails = ({
+  name,
+  desc,
+  numOfPosts,
+  followers,
+  numOfUsers,
+  creator,
+  createdAt
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card_header}>
+        <span className={styles.name}>{name}</span>
+        <Button type='label' text='+ Follow' />
+      </div>
+      <div className={styles.space_desc}>{desc}</div>
+      <SpaceStats
+        numOfPosts={numOfPosts}
+        followers={followers}
+        numOfUsers={numOfUsers}
+      />
+    </div>
+  );
+};
+
+SpaceDetails.propTypes = {
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string,
+  numOfPosts: PropTypes.number,
+  followers: PropTypes.number,
+  numOfUsers: PropTypes.number,
+  creator: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired
+};
+
+SpaceDetails.defaultProps = {
+  desc: undefined,
+  numOfPosts: 0,
+  followers: 0,
+  numOfUsers: 0
+};
+
+export default SpaceDetails;
