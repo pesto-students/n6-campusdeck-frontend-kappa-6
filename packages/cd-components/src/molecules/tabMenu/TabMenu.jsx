b0b0f74e926cf9f70ce21b64a53e44ca8ca63379
PushpakB3096 <rtpushpak@gmail.com>
2021-08-23 13:03:11 +0530

    Add option to make tab menu centered

--- packages/cd-components/src/molecules/tabMenu/TabMenu.jsx
+++ packages/cd-components/src/molecules/tabMenu/TabMenu.jsx
@@ -5,9 +5,9 @@ const { TabPane } = Tabs;
 // styles
 import styles from "./tabMenu.module.scss";
 
-const TabMenu = ({ tabs, callback }) => {
+const TabMenu = ({ tabs, callback, centered }) => {
   return (
-    <Tabs defaultActiveKey='1' onChange={callback}>
+    <Tabs defaultActiveKey='1' onChange={callback} centered={centered}>
       {tabs &&
         tabs.map((tab, idx) => (
           <TabPane tab={tab.label} disabled={tab.disabled} key={idx + 1}>
@@ -20,7 +20,8 @@ const TabMenu = ({ tabs, callback }) => {
 
 TabMenu.propTypes = {
   tabs: PropTypes.array.isRequired,
-  callback: PropTypes.func
+  callback: PropTypes.func,
+  centered: PropTypes.bool
 };
 
 TabMenu.defaultProps = {
@@ -38,7 +39,8 @@ TabMenu.defaultProps = {
   ],
   callback: key => {
     console.log(`Tab clicked: ${key}`);
-  }
+  },
+  centered: false
 };
 
 export default TabMenu;
