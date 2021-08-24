4084230226a4308e89430c3e9ef58f7b3b68906c
PushpakB3096 <rtpushpak@gmail.com>
2021-08-24 21:46:30 +0530

    Add long button size

--- packages/cd-components/src/atoms/button/constants/button.size.js
+++ packages/cd-components/src/atoms/button/constants/button.size.js
@@ -1,11 +1,13 @@
 const SMALL = "small";
 const MEDIUM = "medium";
 const LARGE = "large";
+const LONG = "long";
 
 const BUTTON_SIZE = {
   SMALL,
   MEDIUM,
-  LARGE
+  LARGE,
+  LONG
 };
 
 export default BUTTON_SIZE;
