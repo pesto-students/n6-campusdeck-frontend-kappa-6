41f43c1489afeacfa06721c82de155bc75d939ef
PushpakB3096 <rtpushpak@gmail.com>
2021-08-24 12:07:32 +0530

    Add title input and validations in create post

--- packages/cd-student/src/pages/createPost/CreatePost.jsx
+++ packages/cd-student/src/pages/createPost/CreatePost.jsx
@@ -1,17 +1,82 @@
 import { useState } from "react";
-import { TabMenu, Input } from "@cd/components";
+
+import { TabMenu } from "@cd/components";
+import { POST_TITLE_LIMIT } from "../../constants/post";
 
 // styles
 import styles from "./createPost.module.scss";
 
 const CreatePost = () => {
-  const [remainingChars, setRemainingChars] = useState(300);
-  const [title, setTitle] = useState("");
+  const [remainingChars, setRemainingChars] = useState(POST_TITLE_LIMIT);
+  const [postData, setPostData] = useState({
+    title: ""
+  });
+
+  const validatePost = (val, field) => {
+    switch (field) {
+      case "title":
+        {
+          if (val.length > POST_TITLE_LIMIT) return false;
+        }
+        break;
+      case "body":
+        {
+        }
+        break;
+      case "image":
+        {
+        }
+        break;
+      case "video":
+        {
+        }
+        break;
+      default: {
+        // do nothing
+      }
+    }
+    return true;
+  };
+
+  const handleInput = e => {
+    const val = e.target.value;
+    const field = e.target.name;
+
+    const isValid = validatePost(val, field);
+
+    if (isValid) {
+      setPostData({
+        ...postData,
+        [field]: val
+      });
+    }
+
+    if (field === "title" && isValid) {
+      const valueLen = val.length;
+      setRemainingChars(POST_TITLE_LIMIT - valueLen);
+    }
+  };
+
+  const submit = () => {
+    console.log(postData);
+  };
 
   const TextComponent = (
     <div>
-      <Input placeholder='Title' />
-      <div>{remainingChars}/300</div>
+      <div className={styles.title_section}>
+        <input
+          className={styles.title_input}
+          placeholder='Title'
+          autoComplete='off'
+          value={postData.title}
+          name='title'
+          onChange={handleInput}
+          onBlur={submit}
+        />
+        <div className={styles.remaining_chars}>
+          {remainingChars}/{POST_TITLE_LIMIT}
+        </div>
+      </div>
     </div>
   );
   return (
