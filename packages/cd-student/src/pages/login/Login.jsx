67d4c60e297aca687f37f6cbb3269f8fad8da5fd
PushpakB3096 <rtpushpak@gmail.com>
2021-08-25 14:13:35 +0530

    Add login page

---
+++ packages/cd-student/src/pages/login/Login.jsx
@@ -0,0 +1,32 @@
+import { Banner, Button } from "@cd/components";
+
+// styles
+import styles from "./login.module.scss";
+
+const Login = () => {
+  const submitForm = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.container}>
+      <Banner />
+      <div className={styles.content}>
+        <div className={styles.heading}>Login to your account</div>
+        <form className={styles.form} onSubmit={submitForm}>
+          <input className={styles.input} type='email' placeholder='Email' />
+          <input
+            className={styles.input}
+            type='password'
+            placeholder='Password'
+          />
+          <Button className={styles.login_btn} size='long'>
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
