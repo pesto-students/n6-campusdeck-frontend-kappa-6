ff8eda48ee43ab8f49b2fb8514ad7d448d815164
PushpakB3096 <rtpushpak@gmail.com>
2021-08-28 22:30:02 +0530

    Add correct sidebar links in campus portal

--- packages/cd-campus/src/Base.jsx
+++ packages/cd-campus/src/Base.jsx
@@ -1,6 +1,12 @@
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
-import { Navbar, LeftSidebar } from "@cd/components";
+import {
+  Navbar,
+  LeftSidebar,
+  BarChart2,
+  Users,
+  GlobalOutlined
+} from "@cd/components";
 
 // styles
 import styles from "./base.module.scss";
@@ -18,7 +24,29 @@ const Base = ({ children }) => {
       <Navbar />
       <div className={styles.container}>
         <div className={styles.left_sidebar}>
-          <LeftSidebar onClick={onClick} />
+          <LeftSidebar
+            onClick={onClick}
+            links={[
+              {
+                label: "Insights",
+                destination: "/insights",
+                isActive: true,
+                icon: <BarChart2 />
+              },
+              {
+                label: "Spaces",
+                destination: "/global",
+                isActive: false,
+                icon: <GlobalOutlined />
+              },
+              {
+                label: "Students",
+                destination: "/students",
+                isActive: false,
+                icon: <Users />
+              }
+            ]}
+          />
         </div>
 
         {children}
