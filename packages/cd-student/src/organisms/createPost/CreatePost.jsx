0214c40dbd0285367a1f2fdc6fb3b781b2d5c5c3
PushpakB3096 <rtpushpak@gmail.com>
2021-08-30 20:22:03 +0530

    Make a separate component for text posts

--- packages/cd-student/src/organisms/createPost/CreatePost.jsx
+++ packages/cd-student/src/organisms/createPost/CreatePost.jsx
@@ -1,17 +1,12 @@
 import { useState, useEffect } from "react";
-import { Tooltip, Tag } from "antd";
 
-import { Button, TabMenu, InfoCircleFilled, BUTTON_SIZE } from "@cd/components";
-import { POST_TITLE_LIMIT } from "../../constants/post";
-import QuillEditor from "../quillEditor/QuillEditor";
+import { TabMenu } from "@cd/components";
+import TextPost from "../textPost/TextPost";
 
 // styles
 import styles from "./createPost.module.scss";
 
-const { CheckableTag } = Tag;
-
 const CreatePost = () => {
-  const [remainingChars, setRemainingChars] = useState(POST_TITLE_LIMIT);
   const [postData, setPostData] = useState({
     title: "",
     body: "Start typing...",
@@ -21,42 +16,6 @@ const CreatePost = () => {
     name: "Random",
     tags: ["Info", "Urgent", "Advice", "Question"]
   });
-
-  const validatePost = (val, field) => {
-    switch (field) {
-      case "title":
-        if (val.length > POST_TITLE_LIMIT) return false;
-        break;
-      case "body":
-        break;
-      case "image":
-        break;
-      case "video":
-        break;
-      default:
-      // do nothing
-    }
-    return true;
-  };
-
-  const handleInput = e => {
-    const val = e.target.value;
-    const field = e.target.name;
-
-    const isValid = validatePost(val, field);
-
-    if (isValid) {
-      setPostData({
-        ...postData,
-        [field]: val
-      });
-    }
-
-    if (field === "title" && isValid) {
-      const valueLen = val.length;
-      setRemainingChars(POST_TITLE_LIMIT - valueLen);
-    }
-  };
 
   const submit = () => {
     console.log(postData);
@@ -79,59 +38,6 @@ const CreatePost = () => {
     console.log(postData);
   }, [postData]);
 
-  const TextComponent = (
-    <>
-      <div className={styles.title_section}>
-        <input
-          className={styles.title_input}
-          placeholder='Title'
-          autoComplete='off'
-          value={postData.title}
-          name='title'
-          onChange={handleInput}
-        />
-        <div className={styles.remaining_chars}>
-          {remainingChars}/{POST_TITLE_LIMIT}
-        </div>
-      </div>
-      <div className={styles.body}>
-        <QuillEditor
-          className={styles.body_editor}
-          postData={postData}
-          setPostData={setPostData}
-        />
-      </div>
-
-      <div className={styles.post_labels}>
-        <div className={styles.tag_title}>Select a tag:</div>
-        {space &&
-          space.tags &&
-          space.tags.map(tag => (
-            <CheckableTag
-              className={styles.label}
-              key={tag}
-              checked={tag === postData.tag}
-              onChange={checked => handleTagSelect(tag, checked)}
-            >
-              {tag}
-            </CheckableTag>
-          ))}
-      </div>
-      <div className={styles.options}>
-        <label htmlFor='accessibility'>
-          <input type='checkbox' id='accessibility' />
-          <span style={{ marginLeft: "0.5rem" }}>Make this a public post?</span>
-        </label>
-        <Tooltip
-          title='Selecting this option will make this post visible to anyone who accesses the space.'
-          placement='right'
-          key='geekblue'
-        >
-          <InfoCircleFilled style={{ marginLeft: "0.5rem" }} />
-        </Tooltip>
-      </div>
-    </>
-  );
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -154,7 +60,15 @@ const CreatePost = () => {
             {
               label: "Text",
               disabled: false,
-              content: TextComponent
+              content: (
+                <TextPost
+                  postData={postData}
+                  setPostData={setPostData}
+                  space={space}
+                  removeTag={removeTag}
+                  handleTagSelect={handleTagSelect}
+                />
+              )
             },
             {
               label: "Media",
---
+++ packages/cd-student/src/organisms/textPost/TextPost.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Tooltip, Tag } from "antd";
+
+import { InfoCircleFilled } from "@cd/components";
+import QuillEditor from "../quillEditor/QuillEditor";
+import { POST_TITLE_LIMIT } from "../../constants/post";
+
+// styles
+import styles from "./textPost.module.scss";
+
+const { CheckableTag } = Tag;
+
+const TextPost = ({
+  postData,
+  setPostData,
+  space,
+  removeTag,
+  handleTagSelect
+}) => {
+  const [remainingChars, setRemainingChars] = useState(POST_TITLE_LIMIT);
+
+  const validatePost = (val, field) => {
+    switch (field) {
+      case "title":
+        if (val.length > POST_TITLE_LIMIT) return false;
+        break;
+      case "body":
+        break;
+      case "image":
+        break;
+      case "video":
+        break;
+      default:
+      // do nothing
+    }
+    return true;
+  };
+
+  const handleInput = e => {
+    const val = e.target.value;
+    const field = e.target.name;
+
+    const isValid = validatePost(val, field);
+
+    if (isValid) {
+      setPostData({
+        ...postData,
+        [field]: val
+      });
+    }
+
+    if (field === "title" && isValid) {
+      const valueLen = val.length;
+      setRemainingChars(POST_TITLE_LIMIT - valueLen);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title_section}>
+        <input
+          className={styles.title_input}
+          placeholder='Title'
+          autoComplete='off'
+          value={postData.title}
+          name='title'
+          onChange={handleInput}
+        />
+        <div className={styles.remaining_chars}>
+          {remainingChars}/{POST_TITLE_LIMIT}
+        </div>
+      </div>
+      <div className={styles.body}>
+        <QuillEditor
+          className={styles.body_editor}
+          postData={postData}
+          setPostData={setPostData}
+        />
+      </div>
+
+      <div className={styles.post_labels}>
+        <div className={styles.tag_title}>Select a tag:</div>
+        {space &&
+          space.tags &&
+          space.tags.map(tag => (
+            <CheckableTag
+              className={styles.label}
+              key={tag}
+              checked={tag === postData.tag}
+              onChange={checked => handleTagSelect(tag, checked)}
+            >
+              {tag}
+            </CheckableTag>
+          ))}
+      </div>
+      <div className={styles.options}>
+        <label htmlFor='accessibility'>
+          <input type='checkbox' id='accessibility' />
+          <span style={{ marginLeft: "0.5rem" }}>Make this a public post?</span>
+        </label>
+        <Tooltip
+          title='Selecting this option will make this post visible to anyone who accesses the space.'
+          placement='right'
+          key='geekblue'
+        >
+          <InfoCircleFilled style={{ marginLeft: "0.5rem" }} />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
+
+TextPost.propTypes = {
+  postData: PropTypes.object.isRequired,
+  setPostData: PropTypes.func.isRequired,
+  space: PropTypes.object.isRequired,
+  handleTagSelect: PropTypes.func.isRequired,
+  removeTag: PropTypes.func.isRequired
+};
+
+export default TextPost;
