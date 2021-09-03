c3b50a89eec5c93ad31293c91ccbeb8fb94e211e
PushpakB3096 <rtpushpak@gmail.com>
2021-09-03 11:45:10 +0530

    Adding request interceptor in FE

--- packages/cd-student/src/api/index.js
+++ packages/cd-student/src/api/index.js
@@ -17,6 +17,24 @@ const POST_API = axios.create({
   baseURL: process.env.REACT_APP_POST_SERVICE_URL
 });
 
+// interceptor function that will add token in auth header of each request
+const addToken = req => {
+  const profile = JSON.parse(localStorage.getItem("profile"));
+  const token = profile?.token;
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+};
+
+// adding the interceptor function to all requests for these APIs
+USER_API.interceptors.request.use(addToken);
+CAMPUS_API.interceptors.request.use(addToken);
+SPACE_API.interceptors.request.use(addToken);
+POST_API.interceptors.request.use(addToken);
+
 // user auth related
 export const signUp = formData => USER_API.post("/users/signup", formData);
 export const signIn = formData => USER_API.post("/users/signin", formData);
