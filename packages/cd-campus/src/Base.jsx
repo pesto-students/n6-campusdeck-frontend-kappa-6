aedfdeb76df6a769f6ef6727f988769b64ebe5e8
PushpakB3096 <rtpushpak@gmail.com>
2021-09-10 13:38:02 +0530

    Add logout feature in admin portal

--- packages/cd-campus/src/Base.jsx
+++ packages/cd-campus/src/Base.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import {
   Navbar,
   LeftSidebar,
@@ -8,21 +10,43 @@ import {
   GlobalOutlined
 } from "@cd/components";
 
+import { LOGOUT } from "./actions/constants/actionTypes";
+
 // styles
 import styles from "./base.module.scss";
 
 const Base = ({ children }) => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const history = useHistory();
   const location = useLocation();
+  const dispatch = useDispatch();
 
   // navigate to given page
   const onClick = destination => {
     history.push(destination);
   };
 
+  // function to logout the user
+  const logout = () => {
+    dispatch({
+      type: LOGOUT
+    });
+
+    // clears the current user
+    setUser(() => {
+      // redirect user to home page upon logging out
+      history.push("/login");
+
+      return null;
+    });
+  };
   return (
     <>
-      <Navbar />
+      <Navbar
+        userName={user?.result.name}
+        userImg={user?.result.profileImg}
+        logout={logout}
+      />
       <div className={styles.container}>
         <div className={styles.left_sidebar}>
           <LeftSidebar
